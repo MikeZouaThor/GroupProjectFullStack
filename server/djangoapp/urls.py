9d365842f01267baf3faf64b2f30ae228bff9988
Chinjaa Xiong <chinjaa@Chinjaas-MacBook-Pro.local>
2025-06-11 20:28:31 -0500

    build CarModel and CarMake Django models

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -11,7 +11,9 @@ urlpatterns = [
     # path for login
     path(route='login', view=views.login_user, name='login'),
     # path for logout
-    path(route='logout', view=views.logout_request, name='logout')
+    path(route='logout', view=views.logout_request, name='logout'),
+    # path for get cars 
+    path(route='get_cars', view=views.get_cars, name='get_cars'),
     # path for dealer reviews view
 
     # path for add a review view
